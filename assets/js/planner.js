a54b29ee6b912357debf336e44c8e1c6e5273962
Kevin Geary <kgeary@gmail.com>
2019-12-17 18:26:38 -0600

    Added configurable multi-day support. Updated code to use data-hour attribute to identify timeblocks. Refactored code for conciseness.

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -7,6 +7,17 @@ let curDate = moment(); // Current Day is initially Today
 $(function() {
     // Set the date in the header
     SetCurrentDateLabel();
+
+    // Enable Multi-day support?
+    $("#enableDate").on("click", function(){
+        if($(this).is(":checked")){
+            $("#dateGroup").show(); 
+        } else {
+            $("#dateGroup").hide();
+            curDate = moment();
+            loadDay();
+        }
+    });
     $("#datepicker").val(moment().format('YYYY-MM-DD'));
     $("#datepicker").on("change", function () {
         curDate = moment($(this).val(), "YYYY-MM-DD");
@@ -16,67 +27,70 @@ $(function() {
         $(".container").html("");
         loadDay();
     });
+
+    // Load the day into the view
     loadDay();
 })
 
+// Set's the current day in the header
 function SetCurrentDateLabel() {
     $("#currentDay").text(curDate.format('dddd, MMMM Do'));
 }
 
+// Load the current day onto the page
 function loadDay() {
     // Create and Load Time Blocks
     for(let index=0; index<times.length; index++) {
         $(".container").append(createTimeBlock(times[index]));
     }
 
-    // Setup Save Events
+    // Setup Save Button Events
     $(".saveBtn").on("click", function() {
         var $desc = $(this).siblings(".description");
         let hr = $desc.attr("data-hour");
         let txt = $desc.val();
-        console.log(hr, txt);
         localStorage.setItem(getStoreDatePrefix() + hr.trim(), txt.trim());
         $("#save-toast").fadeIn(750).fadeOut(1500);
     });
 
+    // Setup Interval to Update past, present, future classes periodically (30s)
+    updateInterval = setInterval(checkTimeBlocks, 30000);
+
+    //****************************************
+    // STYLING
+    //****************************************
     // Change opacity of description on hover
     $('.description').hover( function() {
         $(this).toggleClass("active");
     });
-
     // Hover over save button changes opacity and makes disk larger
     $('.saveBtn').hover( function() {
         $(this).toggleClass("active");
     });
 
-    // Update past, present, future classes periodically (30s)
-    updateInterval = setInterval(checkTimeBlocks, 30000);
+    
 }
 
 // Check the timeblocks to see if their tense has changed
+// Go through each hour and compare 
 function checkTimeBlocks() {
-    console.log("CHECK TIME BLOCKS");
-    let $hours = $('.hour');
-    $hours.each(function(index) {
-        let $text = $(this).next();
-        let hour12ampm = $(this).text();
-        let n = moment();
-        let t = getMoment12H(hour12ampm);
-        console.log("TTTT", t);
-        let tense = getTense(n, t);
-        if ($text.hasClass(tense)) {
+    let $descriptions = $('.description');
+    $descriptions.each(function(index) {
+        let hour12 = $(this).attr("data-hour"); // Get the hour
+        let t = getMoment12H(hour12);
+        let tense = getTense(t);
+        if ($(this).hasClass(tense)) {
             //console.log("/NO CHANGE");
         } else if (tense === "present") {
-            $text.removeClass("past future");
+            $(this).removeClass("past future");
         } else if (tense === "past") {
-            $text.removeClass("present future");
+            $(this).removeClass("present future");
         } else if (tense === "future") {
-            $text.removeClass("past present");
+            $(this).removeClass("past present");
         } else {
             alert("Unknown Tense");
         }
-        //console.log("TENSE=", tense);
-        $text.addClass(tense);
+        $(this).addClass(tense);
     });
  }
 
@@ -100,30 +114,36 @@ function createTimeBlock(hour24) {
 // Create a single page element
 function createEl(tag, cls, hour24) {
     let el = document.createElement(tag);
-    el.setAttribute("class", cls);
-    // Special Handling for Hour and Text Columns
+    // Special Handling for Hour and Description Columns
     if (hour24) {
         let t = getMoment24H(hour24);
-        let n = moment();
-        if (tag === "textarea") {
-            cls += " " + getTense(n, t);
+        if (cls.includes("description")) {
+            // description class
             let displayHour = formatAmPm(t);
+            cls += " " + getTense(t);
             el.textContent = localStorage.getItem(getStoreDatePrefix() + displayHour);
             el.setAttribute("data-hour", displayHour);
         } else {
+            // hour class
             el.textContent = formatAmPm(t).padEnd(4, " ");
         }
     }
+    // Set the classes
     el.setAttribute("class", cls);
     return el;
 }
 
-// n = Moment now
-// t = time-block Moment
+// Check to see if the specified time is in the past present or future compared to time now.
+// t = hour moment
 // returns appropriate tense class (past, present, or future)
-function getTense(n, t) {
+function getTense(t) {
     let cls;
-    if (n.isSame(t, "hour") && n.isSame(t, "day") && n.isSame(t, "month") && n.isSame(t, "year")) {
+    let n = moment();
+
+    if (n.isSame(t, "hour") && 
+        n.isSame(t, "day") && 
+        n.isSame(t, "month") && 
+        n.isSame(t, "year")) {
         cls = "present";
     } else if (n.isAfter(t)) {
         cls = "past"
@@ -133,18 +153,28 @@ function getTense(n, t) {
     return cls;
 }
 
+//**********************************
+// GET STRING
+//**********************************
+// Get string prefix for localStorage based off curDate
 function getStoreDatePrefix() {
     return curDate.format("YYYYMMDD-");
 }
 
-function getMoment12H(hour12ampm) { 
-    return moment(curDate.format("YYYYMMDD ") + hour12ampm, "YYYYMMDD hA");
-}
-
-function getMoment24H(hour24) { 
-    return moment(curDate.format("YYYYMMDD ") + hour24, "YYYYMMDD H");
-}
-
+// Return the moment formated as a 12-hour AM/PM time string (Example: 10AM)
 function formatAmPm(m) { 
     return m.format("hA");
+}
+
+//**********************************
+// GET MOMENT
+//**********************************
+// Create a new moment based off curDate and a 12hr AM/PM format time string
+function getMoment12H(hour12) { 
+    return moment(curDate.format("YYYYMMDD ") + hour12, "YYYYMMDD hA");
+}
+
+// Create a new moment based off curDate and a 24hr format time string
+function getMoment24H(hour24) { 
+    return moment(curDate.format("YYYYMMDD ") + hour24, "YYYYMMDD H");
 }
